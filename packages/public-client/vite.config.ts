1a838f1b861310ce6b7b5e92f0e62b358b645d65
Abdalla Alhag <alhagabdalla@gmail.com>
2024-10-17 15:44:58 -0700

    Add redirect file

--- packages/public-client/vite.config.ts
+++ packages/public-client/vite.config.ts
@@ -5,21 +5,6 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { 
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 5173 
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 5173,
-    strictPort: false, // Changed to false for Railway
-  },
-  
-
-    // rollupOptions: {
-    //   input: path.resolve(__dirname, 'src/main.tsx'), // Your entry file
-    //   // external: ['lucide-react', '@tinymce/tinymce-react'], // Add all external modules here
-    // },
   css: {
     postcss: path.resolve(__dirname, './postcss.config.js'), // Ensure PostCSS config is referenced
   },
