0e5f5366032d5dddacfbd86bf1267500b1fe5090
Abdalla Alhag <alhagabdalla@gmail.com>
2024-10-18 17:03:43 -0700

    Change how we check auth in protected mode

--- packages/private-client/src/middleware/ProtectedRoute.tsx
+++ packages/private-client/src/middleware/ProtectedRoute.tsx
@@ -5,14 +5,16 @@ import Cookies from 'js-cookie';
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const navigate = useNavigate();
   // const token = localStorage.getItem('token'); // Check JWT token
-  const token = Cookies.get('token'); // Get the token from cookies
+  // const token = Cookies.get('token'); // Get the token from cookies
+  const isAuthenticated = () => !!Cookies.get('token'); //
 
-  console.log('token is: ', token);
+  // console.log('token is: ', token);
   useEffect(() => {
-    if (!token) {
+    // if (!token) {
+    if (!isAuthenticated()) {
       navigate('/login'); // Redirect to login if not authenticated
     }
-  }, [navigate, token]);
+  }, [navigate]);
 
   return <>{children}</>;
 };
